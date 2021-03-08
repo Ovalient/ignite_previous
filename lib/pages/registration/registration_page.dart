047549663e2ed0ff6593b352d0ffca7d02b04a11
Ovalient <12kljk@gmail.com>
2021-03-08 18:11:09 +0900

    implementing profile search page

--- lib/pages/registration/registration_page.dart
+++ lib/pages/registration/registration_page.dart
@@ -9,7 +9,7 @@ Route _createRoute(Widget page) {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => page,
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = Offset(0.0, 1.0);
+      var begin = Offset(1.0, 0.0);
       var end = Offset.zero;
       var curve = Curves.ease;
 
